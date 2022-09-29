e15c99fab1cf0d5a99c5267a0c973aa7be2db003
Caseyann <goore_44@hotmail.ca>
2022-09-29 10:08:26 -0400

    first commit

---
+++ PickerViewSwitch/ModelController.h
@@ -0,0 +1,16 @@
+//
+//  ModelController.h
+//  PickerViewSwitch
+//
+//  Created by Caseyann Goore on 2022-09-27.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ModelController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PickerViewSwitch/ModelController.m
@@ -0,0 +1,12 @@
+//
+//  ModelController.m
+//  PickerViewSwitch
+//
+//  Created by Caseyann Goore on 2022-09-27.
+//
+
+#import "ModelController.h"
+
+@implementation ModelController
+
+@end
